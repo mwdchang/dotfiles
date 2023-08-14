4858497ab8a7b1817981ac395b1c66db776cafc9
Daniel <mwdchang@gmail.com>
2023-08-13 23:32:13 -0400

    nvim configuration

---
+++ nvim/lua/custom/plugins/barbar.lua
@@ -0,0 +1,19 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  },
+  init = function() vim.g.barbar_auto_setup = false end,
+  opts = {
+    animation = false,
+    tabpages = true,
+    auto_hide = true,
+    insert_at_start = false,
+    insert_at_end = true,
+    minimum_padding = 1,
+    sidebar_filetypes = {
+      ['neo-tree'] = { event = 'BufWipeout' },
+    },
+    no_name_title = '???'
+  }
+}
---
+++ nvim/lua/custom/plugins/filetree.lua
@@ -0,0 +1,30 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      enable_git_status = false,
+      default_component_configs = {
+      },
+      window = {
+        width = 30,
+        mappings = {
+          ["o"] = { "toggle_node" },
+          ["P"] = function(state)
+            local node = state.tree:get_node()
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end
+        }
+      }
+    }
+  end,
+}
