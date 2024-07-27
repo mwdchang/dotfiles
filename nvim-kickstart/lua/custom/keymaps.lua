020be524ad83e9b87cf4b549ef2799171b9e2225
Daniel Chang <mwdchang@gmail.com>
2024-07-26 20:19:09 -0400

    nvim refresh

---
+++ nvim-kickstart/lua/custom/keymaps.lua
@@ -0,0 +1,26 @@
+local options = { noremap = true }
+
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>', options)
+vim.keymap.set('n', '<leader>s', ':w<CR>', options)
+vim.keymap.set('n', '<leader>jq', ':%!jq .<CR>', options)
+vim.keymap.set('n', '<leader>j', ':!python -m json.tool<CR>', options)
+
+-- Buffer movement
+vim.keymap.set('n', '<leader>a', '<Cmd>BufferPrevious<CR>', options)
+vim.keymap.set('n', '<leader>s', '<Cmd>BufferNext<CR>', options)
+
+-- Don't jump to the next match token
+vim.keymap.set('n', '*', '*``', options)
+
+-- Alternate <Escape>
+vim.keymap.set('i', ';;', '<esc>', options)
+
+-- Jump to last edit
+vim.keymap.set('n', '<leader>h', "'.", options)
+
+-- Close everything except the current buffer
+vim.keymap.set('n', '<leader>c', '<Cmd>w | %bd | e#<CR>', options)
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, options)
+
+-- Copy buffer
+vim.keymap.set('n', '<leader>y', 'gg"+yG', options)
