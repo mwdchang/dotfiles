05fa879f38fa47bad387e8092a42a9ee8414be1e
Daniel <mwdchang@gmail.com>
2024-09-02 22:11:55 -0400

    colorscheme

--- nvim-kickstart/lua/custom/colorschemes.lua
+++ nvim-kickstart/lua/custom/colorschemes.lua
@@ -13,4 +13,18 @@ return {
       }
     end,
   },
+  {
+    'EdenEast/nightfox.nvim',
+    config = function()
+      require('nightfox').setup {
+        options = {
+          styles = {
+            comments = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          }
+        }
+      }
+    end,
+  }
 }
