44a23a0326172350f976bde33f58368912da6a3b
Daniel Chang <mwdchang@gmail.com>
2025-08-02 18:04:43 -0400

    add copilot, formatting

--- nvim-kickstart/lua/custom/plugins/barbar.lua
+++ nvim-kickstart/lua/custom/plugins/barbar.lua
@@ -1,24 +1,24 @@
 return {
-	"romgrk/barbar.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	},
-	init = function()
-		vim.g.barbar_auto_setup = false
-	end,
-	opts = {
-		animation = false,
-		tabpages = true,
-		auto_hide = true,
-		insert_at_start = false,
-		insert_at_end = true,
-		minimum_padding = 1,
-		sidebar_filetypes = {
-			NvimTree = true,
-			-- ['neo-tree'] = { event = 'BufWipeout' },
-		},
-		no_name_title = "???",
-	},
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    animation = false,
+    tabpages = true,
+    auto_hide = true,
+    insert_at_start = false,
+    insert_at_end = true,
+    minimum_padding = 1,
+    sidebar_filetypes = {
+      NvimTree = true,
+      -- ['neo-tree'] = { event = 'BufWipeout' },
+    },
+    no_name_title = '???',
+  },
 }
 
 -- return {}
